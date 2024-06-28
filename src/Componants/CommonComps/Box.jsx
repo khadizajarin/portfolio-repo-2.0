88e8d52b0a1660e1b382b96b17516ae26ca335f2
khadizajarin <khadiza131310@gmail.com>
2024-06-28 21:28:53 +0600

    skill section

--- src/Componants/CommonComps/Box.jsx
+++ src/Componants/CommonComps/Box.jsx
@@ -1,26 +1,22 @@
 import PropTypes from 'prop-types';
-// import { motion } from 'framer-motion';
-import "./Box.css"
+import "./Box.css";
 
-const Box = ({ text, subtext }) => {
-    // const percentage = parseInt(text);
-
+const Box = ({text, subtext}) => {
     return (
-        <div className="relative flex flex-col justify-center items-center border-[#A8B2C1] border-[10px] rounded-[18px] w-80 h-36">
-
-                <div className="myButton absolute -inset-[9.6px]  border-[#1E3660] rounded-[18px]" > 
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
+        <div>
+            <div className="relative flex flex-col justify-center items-center border-[#A8B2C1] border-[10px] rounded-[18px] w-[350px] h-[170px]">
+                {/* overlap border */}
+                <div className=" absolute -inset-[9.6px] border-[10px] border-[#1E3660] rounded-[18px]">
                 </div>
 
-            {/* Main content */}
-            <div className= ' bg-[#F4F5F7] flex flex-col justify-center items-center w-full h-full rounded-[8px]'>
-                <div className="text-5xl font-bold text-[#1E3660]">{text}</div>
-                <div className="text-xl font-bold text-[#1E3660]">{subtext}</div>
+
+                {/* Main content */}
+                <div className=' bg-[#F4F5F7] flex flex-col justify-center items-center w-full h-full rounded-[8px]'>
+                    <div className="text-5xl font-bold text-[#1E3660]">{text}</div>
+                    <div className="text-xl font-bold text-[#1E3660]">{subtext}</div>
+                </div>
             </div>
-            
+
         </div>
     );
 };
--- src/Componants/Skills/Skills.jsx
+++ src/Componants/Skills/Skills.jsx
@@ -2,16 +2,48 @@ import Box from "../CommonComps/Box";
 
 
 const Skills = () => {
+
     return (
-        <div className="bg-[#E4E8EB] px-52 py-16 grid justify-items-center grid-cols-2 gap-7">
+        <div className=" h-[700px] bg-[#E4E8EB] px-52 py-16 grid grid-cols-2  justify-center items-center">
 
-            <Box className="" text="75%" subtext="React(Intermediate)"/>
-            <Box className="" text="50%" subtext="React(Intermediate)"/>
-            <Box className="" text="25%" subtext="React(Intermediate)"/>
-            <Box className="" text="100%" subtext="React(Intermediate)"/>
+            {/* box 1 */}
+            <div className="relative h-full grid grid-rows-3 gap-4 items-start pr-7">
+                <div className="row-span-3 flex justify-end ">
+                    <Box  text="75%" subtext="React(Intermediate)"/>
+                </div>
+                {/* connecting line 1 */}
+                <div className="absolute left-[350px] top-[170px] border-l-[10px] border-b-[10px] rounded-bl-[18px]  border-[#1E3660]  h-14 w-80">
+                </div>
+            </div>
 
+            
+            {/* box 2 */}
+            <div className="relative h-full grid grid-rows-2 gap-4 items-end pl-7">
+                <div className="-mt-10 row-span-2 flex justify-start ">
+                    <Box  text="50%" subtext="React(Intermediate)"/>
+                </div>
+                {/* connecting line 2 */}
+                <div className="absolute right-[350px] top-[300px] border-r-[10px] border-b-[10px] rounded-br-[18px]  border-[#1E3660]  h-14 w-80">
+                </div>
+            </div>
 
+            {/* box 3 */}
+            <div className="relative h-full grid grid-rows-2 gap-4 items-start pr-7">
+                <div className=" -mt-10  row-span-2 flex justify-end">
+                    <Box  text="25%" subtext="React(Intermediate) "/>
+                </div>
+                {/* connecting line 3 */}
+                <div className="absolute left-[350px] top-[130px] border-l-[10px] border-b-[10px] rounded-bl-[18px] border-[#1E3660]  h-14 w-80">
+                </div>
+                
+            </div>
 
+            {/* box 4 */}
+            <div className="relative h-full grid grid-rows-2  gap-4 items-end pl-7">
+                <div className="-mt-10 row-span-2 flex justify-start ">
+                    <Box  text="100%" subtext="React(Intermediate)"/>
+                </div>
+            </div>
             
         </div>
     );
