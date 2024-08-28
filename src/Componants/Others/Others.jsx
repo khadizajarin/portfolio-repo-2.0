48fecdd14dfd09bec596ebcbf7ea50d3c36c4b51
khadizajarinroza <khadiza131310@gmail.com>
2024-08-28 23:21:11 +0600

    new section

--- src/Componants/Others/Others.jsx
+++ src/Componants/Others/Others.jsx
@@ -4,7 +4,8 @@ const Others = () => {
     return (
         <div className="bg-white text-center lg:pt-[5rem] pt-[4rem]">
             <TitleBar title={'Some of My Other Skills'}></TitleBar>
-            <div className="text-[#768B9C] font-semibold lg:py-[5rem] py-[3rem]  flex flex-wrap justify-center items-center lg:gap-7 gap-6 lg:text-[1.75rem] text-[1.25rem] lg:px-[18rem] px-8">
+            
+            <div className="text-[#768B9C] font-semibold lg:py-[5rem] py-[3rem] flex flex-wrap justify-center items-center lg:gap-7 gap-6 lg:text-[1.75rem] text-[1.25rem] lg:px-[23rem] px-8">
                 <div>
                     <span className="border-[#768B9C] rounded-full border-4 mr-4 inline-block align-middle w-2 h-2"></span> 
                     Html
@@ -16,6 +17,7 @@ const Others = () => {
                 <div>
                     <span className="border-[#768B9C] rounded-full border-4 mr-4 inline-block align-middle w-2 h-2"></span> 
                     Javascript
+                </div> {/* Moved closing div here */}
                 <div>
                     <span className="border-[#768B9C] rounded-full border-4 mr-4 inline-block align-middle w-2 h-2"></span> 
                     Vscode
@@ -44,10 +46,9 @@ const Others = () => {
             </div>
 
             <div className="flex justify-center ">
-                <hr className="border-[#768B9C] lg:opacity-20 opacity-10 border-2  lg:w-[45rem] w-[20rem]"/>
+                <hr className="border-[#768B9C] lg:opacity-20 opacity-10 border-2 lg:w-[45rem] w-[20rem]"/>
             </div>
 
-        </div>
         </div>
     );
 };
